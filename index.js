e501ca0e25bfd19925cb8148604a743a0f996fe2
Istabraq Kulow <istakulow@gmail.com>
2024-07-10 12:02:16 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName='bob'
+const leastFavoriteCustomer = 'Naomi'
+
+function upperCaseCustomerName(){
+    window.customerName =window.customerName.toUpperCase();
+}
+function setBestCustomer(){
+    window.bestCustomer = 'not bob'
+
+}
+function overwriteBestCustomer(){
+   window.bestCustomer = 'maybe bob'
+}
+function changeLeastFavoriteCustomer(){
+     leastFavoriteCustomer = 'Ista';
+}
